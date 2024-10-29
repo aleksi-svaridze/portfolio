347d25c9cb39690ed556c3d9c5772f730f09a9d7
aleksi-svaridze <aleksisvaridze@outlook.com>
2024-10-29 14:43:03 +0400

    Change angular to react native - 2

--- src/pages/ReactNative.jsx
+++ src/pages/ReactNative.jsx
@@ -8,16 +8,7 @@ import ReactImage from "../images/categories/react-framework-logo.webp";
 import JavascriptImage from "../images/categories/javascript-736401_1280.png";
 
 export const ReactNative = () => {
-  const [angularCardsContent] = useState([
-    {
-      id: 1,
-      imageSrc: WorkItImage,
-      title: "work it",
-      description: "Angular Js, Css, BEM",
-      liveUrl: "https://stunning-fenglisu-ff6366.netlify.app/",
-      gitUrl: "",
-    },
-  ]);
+  const [reactNativeCardsContent] = useState();
   return (
     <div className="md:container md:px-10 xl:px-[85px]">
       <SecondaryHero
@@ -27,13 +18,13 @@ export const ReactNative = () => {
         }
       />
       <div className="container py-24 md:py-[120px] lg:py-[160px] px-6 md:px-0 grid lg:grid-cols-3 gap-y-10 lg:gap-y-8 lg:gap-x-[30px]">
-        {angularCardsContent ? (
-          angularCardsContent.map((item) => (
+        {reactNativeCardsContent ? (
+          reactNativeCardsContent.map((item) => (
             <DesignCards item={item} key={item.id} />
           ))
         ) : (
           <h2 className="font-semibold text-lg">
-            No Angular projects at this time
+            No react Native projects at this time
           </h2>
         )}
       </div>
